a2ff0f750e0248bd189a7bd8e8560becb8372529
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-10-10 12:00:53 +0800

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((num1 < 0 || typeof num1 !== "number") || (num2 < 0 || typeof num2 !== "number")) {
+        return "ERROR";
+    }
+
+    let start = num1 < num2 ? num1 : num2;
+    let end = num1 > num2 ? num1 : num2;
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
