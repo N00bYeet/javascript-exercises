a81fee4edb86042e49e3df40dd68bc49a72ae25a
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-10-10 11:27:29 +0800

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    let elementsToRemove = Array.from(elements);
+    return array.filter(value => !elementsToRemove.includes(value));
 };
 
 // Do not edit below this line
