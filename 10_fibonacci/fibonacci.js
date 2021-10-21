ef3da9a1704c58cac58dddeb57725e13517a7f15
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-10-21 11:47:59 +0800

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num, memo={}) {
+    if (num < 0) {
+        return "OOPS"
+    } else if (num == 0) {
+        return 0;
+    } else if (num == 1) {
+        return 1;
+    } else if (num in memo) {
+        return memo[num];
+    }
 
+    memo[num] = fibonacci(num - 2, memo) + fibonacci(num - 1, memo); 
+    return memo[num];
 };
 
 // Do not edit below this line
