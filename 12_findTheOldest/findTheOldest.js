fed0104ff92aecf7e13fbf84bfdae8b09f2b8d90
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-10-21 13:03:20 +0800

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.sort((a, b) => {
+        let currentYear = new Date().getFullYear();
+        let aAge = 'yearOfDeath' in a ? a.yearOfDeath - a.yearOfBirth : currentYear - a.yearOfBirth;
+        let bAge = 'yearOfDeath' in b ? b.yearOfDeath - b.yearOfBirth : currentYear - b.yearOfBirth;
+        return aAge > bAge ? -1 : 1;
+    })
+    return array[0];
 };
 
 // Do not edit below this line
