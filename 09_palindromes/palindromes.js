35ceedac18dd1a0894676f2e8b23686923a10903
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-10-21 11:28:44 +0800

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let rawPalindrome = "";
+    for (let i = 0; i < str.length; i++) {
+        if (isLetter(str[i])) {
+            rawPalindrome += str[i];
+        }
+    }
+    let filteredPalindrome = rawPalindrome.toLowerCase();
 
+    let half = Math.floor(filteredPalindrome.length / 2);
+    let front = filteredPalindrome.slice(0, half);
+    let back = filteredPalindrome.slice(-half).split("").reverse().join("");
+
+    return front === back;
 };
+
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
